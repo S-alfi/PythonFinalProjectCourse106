cb01fdf88f11c750fab8042fb0462f36196c2436
S-alfi <s.alfi1999.8@gmail.com>
2026-04-26 18:26:08 +0300

    Python_Final_Project_Course106
    Website: https://www.loaded.com

--- python_course106_2026/automation_final_project/loaded_page_object/tests/test_loaded_page_object_test.py
+++ python_course106_2026/automation_final_project/loaded_page_object/tests/test_loaded_page_object_test.py
@@ -11,7 +11,7 @@ from python_course106_2026.automation_final_project.loaded_page_object.pages.loa
 
 
 
-def test_01_loaded_home_page_search_input_box_fill_test(setup_playwright_loaded_website):
+def test_home_page_search_and_dropdown_navigation(setup_playwright_loaded_website):
     """
     Test Case: Fill the search input box and interact with the drop-down menu on the Home Page.
     """
@@ -29,7 +29,7 @@ def test_01_loaded_home_page_search_input_box_fill_test(setup_playwright_loaded_
     loaded_home_page.loaded_set_drop_down_menu()
 
 
-def test_02_loaded_home_page_set_currency_to_usd_test(setup_playwright_loaded_website):
+def test_switch_global_currency_to_usd(setup_playwright_loaded_website):
     """
     Test Case: Set the global site currency to USD on the Home Page.
     """
@@ -46,7 +46,7 @@ def test_02_loaded_home_page_set_currency_to_usd_test(setup_playwright_loaded_we
     loaded_home_page.loaded_set_currency()
 
 
-def test_03_loaded_latest_games_page_search_for_the_top_5_games_test(setup_playwright_loaded_website):
+def test_sort_and_extract_top_five_games_list(setup_playwright_loaded_website):
     """
     Test Case: Navigate to Latest Games page, toggle region, and sort to find the top 5 games.
     """
@@ -68,7 +68,7 @@ def test_03_loaded_latest_games_page_search_for_the_top_5_games_test(setup_playw
     loaded_latest_games_page.loaded_sort_top_5_games()
 
 
-def test_04_loaded_pre_order_page_pre_order_first_item_test(setup_playwright_loaded_website):
+def test_pre_order_first_available_item_with_region_toggle(setup_playwright_loaded_website):
     """
     Test Case: Navigate to Pre-Order page, toggle region, and pre-order the first item.
     """
@@ -90,7 +90,7 @@ def test_04_loaded_pre_order_page_pre_order_first_item_test(setup_playwright_loa
     loaded_pre_order_page.loaded_pre_order_first_item()
 
 
-def test_05_loaded_help_center_page_search_test(setup_playwright_loaded_website):
+def test_help_center_search_functionality(setup_playwright_loaded_website):
     """
     Test Case: Navigate to Help Center page and perform a search.
     """
